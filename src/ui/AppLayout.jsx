8c8fa3131fe079399cd16a652b62a9258e126cbd
Wojciech Sadowski <wojciechnwd@gmail.com>
2026-01-12 21:17:24 +0100

    Initial commit

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,27 @@
+import { Outlet, useNavigation } from "react-router-dom";
+import CartOverview from "../features/cart/CartOverview";
+import Header from "./Header";
+import Loader from "./Loader";
+import Order from "../features/order/Order";
+
+function AppLayout() {
+	const navigation = useNavigation();
+	const isLoading = navigation.state === "loading";
+
+	return (
+		<div className="layout">
+			{isLoading && <Loader />}
+
+			<Header />
+
+			<main>
+				<h1>Content - AppLayout</h1>
+				<Outlet />
+			</main>
+
+			<CartOverview />
+		</div>
+	);
+}
+
+export default AppLayout;
