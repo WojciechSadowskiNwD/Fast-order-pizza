c94aa7d561cdbfe8b8b96e39b5a9b0b1b6d7a031
Wojciech Sadowski <wojciechnwd@gmail.com>
2026-01-13 22:59:42 +0100

    Add Button and ButtonLink components, styling, create new @layer

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -2,26 +2,27 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function SearchOrder() {
-	const [query, setQuery] = useState("");
-    const navigate = useNavigate();
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
 
-    function handleSubmit(e){
-        e.preventDefault();
-        if(!query) return
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!query) return;
 
-        navigate(`/order/${query}`);
-        setQuery('');
-    }
+    navigate(`/order/${query}`);
+    setQuery("");
+  }
 
-	return (
-		<form onSubmit={handleSubmit}>
-			<input
-				placeholder="Search order #"
-				value={query}
-				onChange={(e) => setQuery(e.target.value)}
-			/>
-		</form>
-	);
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Search order #"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 sm:focus:w-72 focus:ring focus:ring-yellow-500 focus:outline-none sm:w-64"
+      />
+    </form>
+  );
 }
 
 export default SearchOrder;
